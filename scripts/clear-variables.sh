3da6a9fc2479e11d21d144e33e47462c8a27c87c
do9iigane <dokumegane@interestic.com>
2016-04-18 19:29:34 +0900

    ytake/gardeningに載せ替え

--- scripts/clear-variables.sh
+++ scripts/clear-variables.sh
@@ -1,4 +1,4 @@
-# Clear The Old Environment Variables
+#!/usr/bin/env bash
 
-sed -i '/# Set Homestead Environment Variable/,+1d' /home/vagrant/.profile
-sed -i '/env\[.*/,+1d' /etc/php/7.0/fpm/php-fpm.conf
+sed -i '/# Set Gardening Environment Variable/,+1d' /home/vagrant/.profile
+sed -i '/env\[.*/,+1d' /etc/php-fpm.d/www.conf
